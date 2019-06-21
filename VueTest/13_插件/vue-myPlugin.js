eabf9dbbacc89b0d6718a0e8af3654c0a61474dd
easonyes <2234148120@qq.com>
2019-06-21 18:16:24 +0800

    第一阶段完成

--- VueTest/13_插件/vue-myPlugin.js
+++ VueTest/13_插件/vue-myPlugin.js
@@ -1,3 +1,4 @@
+/*
 (function (window) {
   const MyPlugin = {}
   MyPlugin.install = function (Vue, options) {
@@ -17,4 +18,39 @@
     }
   }
   window.MyPlugin = MyPlugin
+})(window)
+*/
+
+(function (window) {
+  // 需要向外暴露的插件对象
+  const MyPlugin = {};
+
+  MyPlugin.install = function (Vue, options) {
+    // 1. 添加全局方法或属性
+    Vue.myGlobalMethod = function () {
+      // 逻辑...
+      console.log('Vue函数对象的方法myGlobalMethod（）');
+    }
+
+    // 2. 添加全局资源
+    Vue.directive('my-directive', function (el, binding) {
+      el.textContent = binding.value.toUpperCase();
+    })
+
+    // 3. 注入组件选项
+    // Vue.mixin({
+    //   created: function () {
+    //     // 逻辑...
+    //   }
+    //   ...
+    // })
+
+    // 4. 添加实例方法
+    Vue.prototype.$myMethod = function (methodOptions) {
+      // 逻辑...
+      console.log('Vue实例对象的方法$myMethod()');
+    }
+  }
+  // 向外暴露
+  window.Myplugin = MyPlugin;
 })(window)
